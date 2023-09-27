75db3182022a5943c20ab8a5aabf7fdae17a9dbf
LeLiu <le.liu@alumnos.upm.es>
2023-09-27 23:02:28 +0200

    enhanced Fraction #7

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -1,35 +1,12 @@
-
-/**
- * Conceptos: Las fracciones propias son aquellas cuyo numerador es menor que el denominador
- * <p>
- * Las fracciones impropias son aquellas cuyo numerador es mayor que el denominador
- * <p>
- * Dos fracciones son equivalentes cuando el producto de extremos (numerador de la primera por denominador de la segunda) es igual al
- * producto de medios (denominador de la primera por el numerador de la segunda)
- * <p>
- * Las fracciones irreducibles son aquellas que no se pueden simplificar, esto sucede cuando el numerador y el denominador son primos entre
- * sí
- * <p>
- * Reducir varias fracciones a común denominador consiste en convertirlas en otras equivalentes que tengan el mismo denominador
- * <p>
- * Comparar fracciones
- * <p>
- * Suma fracciones: En primer lugar se reducen los denominadores a común denominador, y se suman o se restan los numeradores de las
- * fracciones equivalentes obtenidas
- * <p>
- * Multiplicación: La multiplicación de dos fracciones es otra fracción que tiene: Por numerador el producto de los numeradores. Por
- * denominador el producto de los denominadores.
- * <p>
- * La división de dos fracciones es otra fracción que tiene: Por numerador el producto de los extremos. Por denominador el producto de los
- * medios. Invertir fraccion
- */
 public class Fraction {
+    // ... (previously defined fields and constructors)
     private int numerator;
     private int denominator;
 
-    public Fraction(int numerator, int denominator) {
-        this.numerator = numerator;
-        this.denominator = denominator;
+    // Additional constructor for creating fractions from a single integer (whole number)
+    public Fraction(int wholeNumber) {
+        this.numerator = wholeNumber;
+        this.denominator = 1;
     }
 
     public Fraction() {
@@ -63,4 +40,42 @@ public class Fraction {
                 ", denominator=" + denominator +
                 '}';
     }
+
+    // Method to check if the fraction is proper (numerator < denominator)
+    public boolean isProper() {
+        return numerator < denominator;
+    }
+
+    // Method to check if the fraction is improper (numerator >= denominator)
+    public boolean isImproper() {
+        return numerator >= denominator;
+    }
+
+    // Method to check if two fractions are equivalent
+    public boolean isEquivalent(Fraction other) {
+        return (numerator * other.denominator) == (denominator * other.numerator);
+    }
+
+    // Method to add two fractions and return a new Fraction object
+    public Fraction add(Fraction other) {
+        int commonDenominator = denominator * other.denominator;
+        int newNumerator = (numerator * other.denominator) + (other.numerator * denominator);
+        return new Fraction(newNumerator, commonDenominator);
+    }
+
+    // Method to multiply two fractions and return a new Fraction object
+    public Fraction multiply(Fraction other) {
+        int newNumerator = numerator * other.numerator;
+        int newDenominator = denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    // Method to divide two fractions and return a new Fraction object
+    public Fraction divide(Fraction other) {
+        int newNumerator = numerator * other.denominator;
+        int newDenominator = denominator * other.numerator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    // ... (previously defined methods, including getters, setters, decimal(), and toString())
 }
