30608efae17e6bee142453d41ddd645beb1ff92a
LeLiu <le.liu@alumnos.upm.es>
2023-09-29 01:33:44 +0200

    Fix Search #14

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -1,14 +1,12 @@
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Stream;
-import java.util.stream.Collectors;
 public class Search {
 
     public Stream<String> findUserFamilyNameInitialBySomeProperFraction() {
         return new UsersDatabase().findAll()
                 .flatMap(user -> user.getFractions().stream()
-                        .filter(fraction -> fraction.isProper())
+                        .filter(Fraction::isProper)
                         .map(fraction -> user.getFamilyName().substring(0, 1))
                 )
                 .distinct();
@@ -18,16 +16,14 @@ public class Search {
         List<Fraction> fractions = new UsersDatabase().findAll()
                 .filter(user -> user.getFamilyName().equals(familyName))
                 .flatMap(user -> user.getFractions().stream())
-                .collect(Collectors.toList());
+                .toList();
 
         if (fractions.isEmpty()) {
             throw new IllegalArgumentException("No user found with the given family name.");
         }
 
-        Fraction multiplicationResult = fractions.stream()
+        return fractions.stream()
                 .reduce(new Fraction(1, 1), Fraction::multiply);
-
-        return multiplicationResult;
     }
 
     public Stream<String> findUserFamilyNameByAllNegativeSignFractionDistinct() {
